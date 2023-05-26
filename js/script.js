d5334f173c35048de9b6b8459b9b63e1a71e2034
Rieko00 <ariekbudianto@gmail.com>
2023-05-26 13:58:12 +0700

    new file:   Tugas Apikasi.rar 	modified:   css/style.css 	deleted:    font/Bebas_Neue.zip 	deleted:    font/Dancing_Script.zip 	new file:   font/FuturaTSNew.ttf 	deleted:    font/Merriweather.zip 	deleted:    font/Montserrat.zip 	new file:   font/OpenSans.ttf 	deleted:    font/Pacifico.zip 	deleted:    font/Poppins.zip 	modified:   index.html 	modified:   js/script.js 	modified:   reservasi.html changes some html and css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function scrollToTargetAdjusted() {
     behavior: "smooth",
   });
 }
+
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  var element = document.querySelector(".fadeInUp");
+  if (isElementInViewport(element)) {
+    element.classList.add("animated");
+  }
+}
+
+// Attach scroll event listener
+window.addEventListener("scroll", handleScroll);
