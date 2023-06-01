d2eff422b1c842c8962019899340f18b3f6d4c5d
Rieko00 <ariekbudianto@gmail.com>
2023-06-01 16:14:05 +0700

    modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-function scrollToTargetAdjusted() {
-  var element = document.getElementById("targetElement");
-  var headerOffset = 45;
-  var elementPosition = element.getBoundingClientRect().top;
-  var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-
-  window.scrollTo({
-    top: offsetPosition,
-    behavior: "smooth",
-  });
-}
-
 // Function to check if an element is in the viewport
 function isElementInViewport(element) {
   var rect = element.getBoundingClientRect();
